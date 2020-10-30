1f1f9c12759df3c7c00cf16f62403813f5927c5f
Aleksei Smirnov <alekseiismirnovv@gmail.com>
2020-10-30 18:26:02 +0300

    Update WriteNoteViewController.swift

--- Smart/Source/WriteNote/ViewController/WriteNoteViewController.swift
+++ Smart/Source/WriteNote/ViewController/WriteNoteViewController.swift
@@ -13,6 +13,10 @@ import RealmSwift
 import VinchyCore
 import StringFormatting
 
+fileprivate enum C {
+  static let placeholder: String = localized("general_impression").firstLetterUppercased()
+}
+
 final class WriteNoteViewController: UIViewController {
 
   // MARK: - Interanal Properties
@@ -94,14 +98,64 @@ extension WriteNoteViewController: UITextViewDelegate {
   func textViewDidChange(_ textView: UITextView) {
     interactor?.didChangeNoteText(textView.text)
   }
+
+  func textView(
+    _ textView: UITextView,
+    shouldChangeTextIn range: NSRange,
+    replacementText text: String)
+    -> Bool
+  {
+
+    // Combine the textView text and the replacement text to
+    // create the updated text string
+    let currentText: String = textView.text
+    let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+
+    // If updated text view will be empty, add the placeholder
+    // and set the cursor to the beginning of the text view
+    if updatedText.isEmpty {
+
+      textView.text = C.placeholder
+      textView.textColor = .blueGray
+
+      textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+    }
+
+    // Else if the text view's placeholder is showing and the
+    // length of the replacement string is greater than 0, set
+    // the text color to black then set its text to the
+    // replacement string
+    else if textView.textColor == .blueGray && !text.isEmpty {
+      textView.textColor = UIColor.black
+      textView.text = text
+    }
+
+    // For every other case, the text should change with the usual
+    // behavior...
+    else {
+      return true
+    }
+
+    // ...otherwise return false since the updates have already
+    // been made
+    return false
+  }
 }
 
 // MARK: - WriteNoteViewControllerProtocol
 
 extension WriteNoteViewController: WriteNoteViewControllerProtocol {
+  
   func update(viewModel: WriteNoteViewModel) {
     if let noteText = viewModel.noteText, !noteText.isEmpty {
       textView.text = noteText
+    } else {
+      textView.text = C.placeholder
+      textView.textColor = .blueGray
+
+      textView.becomeFirstResponder()
+
+      textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
     }
     navigationItem.title = viewModel.navigationText
   }
