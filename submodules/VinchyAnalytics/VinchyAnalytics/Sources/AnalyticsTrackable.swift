714a40306d5d81068805eaf708fde7c6a06b9a06
Aleksei <alekseiismirnovv@gmail.com>
2021-02-19 20:10:07 +0300

    some changes

--- submodules/VinchyAnalytics/VinchyAnalytics/Sources/AnalyticsTrackable.swift
+++ submodules/VinchyAnalytics/VinchyAnalytics/Sources/AnalyticsTrackable.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 public protocol AnalyticsTrackable {
-    func trackEvent(_ event: String, params: [String: Any]?, trackers: [Tracker])
+  func trackEvent(_ event: String, params: [String: Any]?, trackers: [Tracker])
 }
 
 public extension AnalyticsTrackable {
-    func trackEvent(_ event: String, params: [String: Any]? = nil, trackers: [Tracker] = Tracker.allCases) {
-        trackers.forEach { tracker in
-            tracker.trackEvent(event, params: params, trackers: [tracker])
-        }
+  func trackEvent(_ event: String, params: [String: Any]? = nil, trackers: [Tracker] = Tracker.allCases) {
+    trackers.forEach { tracker in
+      tracker.trackEvent(event, params: params, trackers: [tracker])
     }
+  }
 }
--- submodules/VinchyAnalytics/VinchyAnalytics/Sources/Tracker.swift
+++ submodules/VinchyAnalytics/VinchyAnalytics/Sources/Tracker.swift
@@ -8,17 +8,17 @@
 import FirebaseAnalytics
 
 public enum Tracker: CaseIterable, AnalyticsTrackable {
-    case firebase
-    
-    public func trackEvent(_ event: String, params: [String: Any]?, trackers: [Tracker]) {
-//        #if RELEASE
-        trackers.forEach { tracker in
-            switch tracker {
-            case .firebase:
-                print(event, params)
-//                Analytics.logEvent(event, parameters: params)
-            }
-        }
-//        #endif
+  case firebase
+  
+  public func trackEvent(_ event: String, params: [String: Any]?, trackers: [Tracker]) {
+    //        #if RELEASE
+    trackers.forEach { tracker in
+      switch tracker {
+      case .firebase:
+        print(event, params)
+      //                Analytics.logEvent(event, parameters: params)
+      }
     }
+    //        #endif
+  }
 }
